4cc0fab36cd107e3094a3fd73b27846e97e31d7c
nguyenha <nguyenthuha1342001@gmail.com>
2023-06-22 17:26:35 +0700

    update 5:26

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -78,7 +78,7 @@ const Nav = () => {
             padding: 0.8rem 1.4rem;
         }
 
-        @media (max-width: ${({ theme }) => theme.media.mobile}){
+        @media (max-width: ${({ theme }) => theme.colors.mobile}){
             .mobile-navbar-btn {
                 display: inline-block;
                 z-index: 9999;
